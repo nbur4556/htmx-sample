3e7f88c0b2846ec4699a456c532e675c1df54ffc
nbur4556 <nbur4556@gmail.com>
2023-07-07 15:46:19 -0500

    create an answer enpoint recieving answer data as a query

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import fetch from 'node-fetch';
 const app = express();
 const port = 3000;
 
+const buildQuestionSnippet = (question, answer) => `
+  <p>${question}</p>
+
+  <label>
+    Answer
+    <input type="text" />
+    <button hx-put='/answer?answer=${answer}'>Submit</button>
+  </label>
+`;
+
 app.use(express.static('./public'));
 
 app.get('/random', async (req, res) => {
   const response = await (await fetch('https://jservice.io/api/random')).json();
-  console.log(response);
+  //TODO: Clean the answer before  sending to snippet
+  const questionSnippet = buildQuestionSnippet(response[0].question, response[0].answer);
+  res.send(questionSnippet)
+});
 
-  const htmlSnippet = `
-    <p>${response[0].question}</p>
-    <label>
-      Answer
-      <input type="text" />
-      <button data-answer="${response[0].answer}">Submit</button>
-    </label>
-  `
-  res.send(htmlSnippet)
+app.put('/answer', (req, res) => {
+  const query = req.query;
 });
 
 app.listen(port, () => {

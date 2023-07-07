e4678cef98f794dbc341000fa6fc89f22a247ae7
nbur4556 <nbur4556@gmail.com>
2023-07-07 15:10:29 -0500

    create endpoint for random trivia questions

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/random", (req, res) => {
+  const htmlSnippet = "<p>Random question</p>"
+  res.send(htmlSnippet)
+});
+
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
